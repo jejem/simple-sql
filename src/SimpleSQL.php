0e1620018155f3b5f73990235885df526b08e24c
Jérémy Desvages <jejem@phyrexia.org>
2015-11-07 20:31:13 +0100

    Releasing v1.4.0, which adds support for master/slave SQL connections (compatible with multiple slaves :))

--- src/SimpleSQL.php
+++ src/SimpleSQL.php
@@ -6,7 +6,7 @@
  * @author Frédéric Le Barzic <fred@lebarzic.fr>
  * @copyright Jérémy 'Jejem' Desvages
  * @license The MIT License (MIT)
- * @version 1.3.0
+ * @version 1.4.0
 **/
 
 namespace Phyrexia\SQL;
