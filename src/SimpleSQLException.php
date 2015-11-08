dab076032015b5e10c0d29fcdb2828a4aa78881f
Jérémy Desvages <jejem@phyrexia.org>
2015-11-08 14:50:48 +0100

    Releasing v1.4.1

--- src/SimpleSQLException.php
+++ src/SimpleSQLException.php
@@ -5,7 +5,7 @@
  * @author Frédéric Le Barzic <fred@lebarzic.fr>
  * @copyright Jérémy 'Jejem' Desvages
  * @license The MIT License (MIT)
- * @version 1.4.0
+ * @version 1.4.1
 **/
 
 namespace Phyrexia\SQL;
